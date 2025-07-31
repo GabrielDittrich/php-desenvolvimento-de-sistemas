405525b3875d3d34c0c7ba2f28c3f6b5f109ae86
GabrielDittrich <gdittrchcj@gmail.com>
2025-07-31 15:23:01 -0300

    Iniciando Bootstrap

--- aula07/editar.php
+++ aula07/editar.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 09 - Parte 02 - Editar Cliente</title>
+    <title>Aula 07 - Parte 02 - Editar Cliente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body class="container-fluid">
 
-    <h1>Aula 09 - Parte 02 - Editar Cliente</h1>
+    <h1>Aula 07 - Parte 02 - Editar Cliente</h1>
     <?php 
     
         require 'menu.php';
--- aula07/salvar.php
+++ aula07/salvar.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 09 - Salvar Cliente</title>
+    <title>Aula 07 - Salvar Cliente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body class="container-fluid">
 
-    <h1>Aula 09 - Salvar Cliente</h1>
+    <h1>Aula 07 - Salvar Cliente</h1>
 
     <p>
         <a href="index.php">Voltar à home</a>
